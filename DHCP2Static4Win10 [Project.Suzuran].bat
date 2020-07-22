973e6bc5cc46b3888f0a79765690e2a60e78cf93
Kazakiri Hikaru <32662755+KazakiriHikaru@users.noreply.github.com>
2020-07-23 02:48:12 +0800

    DHCP2Static4Win10 [Project.Suzuran]

---
+++ DHCP2Static4Win10 [Project.Suzuran].bat
@@ -0,0 +1,16 @@
+@echo off
+title DHCP2Static4Win10 [Project.Suzuran] by Kazakiri Hikaru / @lphaVersion / 2020.07.23
+for /f "tokens=16,*" %%i in ('ipconfig ^| find /i "IPv4"') do set IPv4=%%i
+echo %IPv4%
+for /f "tokens=15,*" %%m in ('ipconfig ^| find /i "掩码"') do set Mask=%%m
+echo %Mask%
+for /f "tokens=15,*" %%g in ('ipconfig ^| find /i "网关"') do set Gateway=%%g
+echo %Gateway%
+echo 确认？
+pause
+echo 正在转换并将网关设定为DNS
+netsh interface ip set address "以太网" static %IPv4% %Mask% %Gateway%
+netsh interface ip set dns "以太网" static %Gateway% validate=no
+echo 设定完成！
+ipconfig -all
+pause
